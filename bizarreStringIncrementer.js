404f9d23dc1600fc2d657816dd21624985ae9941
Tejas H <tejas007tez@gmail.com>
2020-12-14 12:45:17 +0530

    Working solution with all passing tests.

--- bizarreStringIncrementer.js
+++ bizarreStringIncrementer.js
@@ -1,11 +1,33 @@
-// Start your implementation here
 function bizarreStringIncrementer(str) {
-  const lastChar = str[str.length - 1];
+  const lastChar = Number(str[str.length - 1]);
   if (isNaN(lastChar)) {
     return str + "1";
   }
 
-  return 2;
+  if (lastChar < 9) {
+    return str.slice(0, -1) + (lastChar + 1);
+  }
+
+  let minimumReplacableDigits = 1;
+  for (let i = str.length - 2; i >= 0; i--) {
+    const iChar = Number(str[i]);
+
+    if (isNaN(iChar)) {
+      break;
+    }
+    if (iChar === 0) {
+      minimumReplacableDigits++;
+      break;
+    }
+    if (iChar === 9) {
+      minimumReplacableDigits++;
+    }
+  }
+
+  return (
+    str.slice(0, -minimumReplacableDigits) +
+    (Number(str.substr(-minimumReplacableDigits)) + 1)
+  );
 }
 
 module.exports = { bizarreStringIncrementer };
