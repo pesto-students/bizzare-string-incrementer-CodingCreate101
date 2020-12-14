42736b25357efdbf99e1b15a934764adbf3c743f
Tejas H <tejas007tez@gmail.com>
2020-12-14 10:30:18 +0530

    Early return for non numeric last char

--- bizarreStringIncrementer.js
+++ bizarreStringIncrementer.js
@@ -1,5 +1,10 @@
 // Start your implementation here
 function bizarreStringIncrementer(str) {
+  const lastChar = str[str.length - 1];
+  if (isNaN(lastChar)) {
+    return str + "1";
+  }
+
   return 2;
 }
 
